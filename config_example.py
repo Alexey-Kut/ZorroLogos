27701b51314e9f57684b9d08a6ddb18cde9562b2
Alexey Kutyanin <akutyanin@gmail.com>
2025-04-30 14:26:52 +0300

    Добавлен проект ZorroLogos

---
+++ config_example.py
@@ -0,0 +1,5 @@
+iam_token =  "you token here"
+catalog_id = "id here"
+
+url_1 = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
+url_2 = "https://llm.api.cloud.yandex.net:443/operations"
